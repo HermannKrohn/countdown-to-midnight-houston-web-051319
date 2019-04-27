74e97e500417b08121dacf07bc6ae469cff41bfa
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-04-27 20:38:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,4 +9,10 @@ def countdown(interger)
 end
 
 def countdown_with_sleep(integer)
-  
+  while integer >= 0 
+    puts "#{integer} SECOND(S)!"
+    integer -= 1 
+    sleep(1)
+  end 
+  "HAPPY NEW YEAR!"
+end
